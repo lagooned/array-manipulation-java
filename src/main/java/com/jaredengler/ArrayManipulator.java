b2eebef620aa536065898bfca751c4ee08cdeeaa
Jared Engler <jared.lite@gmail.com>
2020-12-27 20:56:42 -0600

    impl algo

--- src/main/java/com/jaredengler/ArrayManipulator.java
+++ src/main/java/com/jaredengler/ArrayManipulator.java
@@ -1,11 +1,34 @@
 package com.jaredengler;
 
 public class ArrayManipulator {
+
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
 
-  public int manipulate(int i, int[][] js) {
-    return 0;
+  public int manipulate(int n, int[][] js) {
+    int[] numbers = new int[n];
+    for (int i = 0; i < js.length; i++) numbers[i] = 0;
+
+    int max = 0;
+    for (int i = 0; i < js.length; i++) {
+      max = mapAndMax(js[i][0], js[i][1], js[i][2], numbers);
+    }
+
+    return max;
   }
+
+  int mapAndMax(int a, int b, int k, int[] js) {
+    int currentMax = 0;
+    for (int i = 0; i < js.length; i++) {
+      if (i >= a - 1 && i <= b - 1) {
+        js[i] = js[i] + k;
+      }
+      if (currentMax < js[i]) {
+        currentMax = js[i];
+      }
+    }
+    return currentMax;
+  }
+
 }
--- src/test/java/com/jaredengler/ArrayManipulatorShould.java
+++ src/test/java/com/jaredengler/ArrayManipulatorShould.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 public class ArrayManipulatorShould {
@@ -14,5 +15,32 @@ public class ArrayManipulatorShould {
   @Test
   void returnsAnInt() {
     assertThat(arrayManipulator.manipulate(0, new int[0][0]), isA(int.class));
+  }
+
+  @Test
+  void mapAndMaxManipulatesArray() {
+    int[] array = new int[]{0,0,0,0,0,0,0,0};
+    arrayManipulator.mapAndMax(1, 7, 1, array);
+    assertThat(array, is(new int[]{1,1,1,1,1,1,1,0}));
+  }
+
+  @Test
+  void mapAndMaxFindsMax() {
+    int[] array = new int[]{0,0,0,0,0,0,0,0};
+    int max = arrayManipulator.mapAndMax(1, 7, 1, array);
+    assertThat(array, is(new int[]{1,1,1,1,1,1,1,0}));
+    assertThat(max, is(1));
+  }
+
+  @Test
+  void manipulateReturnsCorrectValue() {
+    int[][] input = new int[][] {
+      new int[] {1,5,3},
+      new int[] {4,8,7},
+      new int[] {6,9,1},
+    };
+    int n = 10;
+
+    assertThat(arrayManipulator.manipulate(n, input), is(10));
   }
 }
