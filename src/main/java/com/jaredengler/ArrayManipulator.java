509f31e1790ae7d5ce0b2af73bfcce7b934f9719
Jared Engler <jared.lite@gmail.com>
2020-12-27 20:16:48 -0600

    add type-satisfying base case

--- src/main/java/com/jaredengler/ArrayManipulator.java
+++ src/main/java/com/jaredengler/ArrayManipulator.java
@@ -4,4 +4,8 @@ public class ArrayManipulator {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
+
+  public int manipulate(int i, int[][] js) {
+    return 0;
+  }
 }
--- src/test/java/com/jaredengler/ArrayManipulatorShould.java
+++ src/test/java/com/jaredengler/ArrayManipulatorShould.java
@@ -2,13 +2,17 @@ package com.jaredengler;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
 public class ArrayManipulatorShould {
+
+  ArrayManipulator arrayManipulator = new ArrayManipulator();
+
   @Test
-  void add_two_numbers() {
-    assertThat(1 + 1, is(equalTo(2)));
+  void returnsAnInt() {
+    assertThat(arrayManipulator.manipulate(0, new int[0][0]), isA(int.class));
   }
 }
